789789872ce752bf751dabffa756796d9cba5356
Roshan Kumar <roshankunaguero09@gmail.com>
2019-12-29 14:10:58 +0530

    Updates

---
+++ precache-manifest.fb4c5745972199a469800990fcf81c25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef88b5aa9e14f2b701c128fff7124d91",
+    "url": "/monster-react-app/index.html"
+  },
+  {
+    "revision": "fafa738a9cf0d83ec422",
+    "url": "/monster-react-app/static/css/main.57fdaf03.chunk.css"
+  },
+  {
+    "revision": "77115cfbcec72719740d",
+    "url": "/monster-react-app/static/js/2.7db90bec.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monster-react-app/static/js/2.7db90bec.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fafa738a9cf0d83ec422",
+    "url": "/monster-react-app/static/js/main.6edbc3f7.chunk.js"
+  },
+  {
+    "revision": "45eb036408fd857877ea",
+    "url": "/monster-react-app/static/js/runtime-main.e1d2de64.js"
+  }
+]);
